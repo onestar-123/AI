4c4fec7a316b53d2fd384a3250896146b10e7e76
onestar-123 <onestar020610@naver.com>
2026-03-07 13:41:37 +0900

    LAB2 과제

---
+++ LAB (2-1 ~ 2-10) 202113289 이한별.py
@@ -0,0 +1,77 @@
+#LAB 2-1
+print('나의 이름은 :', '홍길동')
+print('나의 나이는 :', 27)
+print('나의 키는', 179, 'cm 입니다.')
+print('10 + 20 =', 10 + 20)
+print('10 * 20 =', 10 * 20)
+
+#LAB 2-4
+print('\n')
+name = '전우치'
+print('나의 이름은 :', name)
+age = 27
+print('나의 나이는 :', age)
+height =179
+print('나의 키는', height, 'cm 입니다.')
+sum = 10 + 20
+print('10 + 20 =', sum)
+mult = 10 * 20
+print('10 * 20 =', mult)
+
+#LAB 2-6
+print('\n')
+width = 20
+height =40
+width =30
+area = width * height
+print('사각형의 면적', area)
+
+#LAB 2-7
+print('\n')
+print('123 * 456 =', 123 * 456)
+print('1357 + 2468 =', 1357 + 2468)
+print('5 ** 4 =', 5 ** 4)
+print('10 / 4 =', 10 / 4)
+print('10 // 5 =', 10 // 5)
+print('10 % 5 =', 10 % 5)
+print('5를 2로 나눈 나머지는', 5 % 2)
+print('2의 제곱근은', 2 ** 0.5, '3의 제곱근은', 3 ** (1/2))
+
+#LAB 2-8
+print('\n')
+a = 8 + 2j
+b = 4 + 3j
+print('a + b =', a + b)
+print('a - b =', a - b)
+print('a * b =', a * b)
+print('a / b =', a / b)
+
+#LAB 2-9
+print('\n')
+a=1024
+print('a >> 1 =', a >> 1)
+print('a >> 2 =', a >> 2)
+a = a >> 1
+print(a)
+a = a >> 1
+print(a)
+a = 1
+print(a << 1)
+a = a << 1
+print(a)
+a = a  << 1
+print(a)
+a = a << 1
+print(a)
+a = a << 1
+print(a)
+
+#LAB 2-10
+print('\n')
+name = input('이름을 입력하세요 : ')
+print(name, '님이 입장하셨습니다.')
+m = int(input('숫자 m을 입력하세요 :'))
+n = int(input('숫자 n을 입력하세요 :'))
+print('m + n =', m + n)
+print('m - n =', m - n)
+
