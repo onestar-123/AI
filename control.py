c699a81ab0c07c419f3ba261608634b84cda96c5
onestar-123 <onestar020610@naver.com>
2026-03-20 11:10:01 +0900

    3rd Lecture

---
+++ control.py
@@ -0,0 +1,23 @@
+"""
+num = 100
+print('num = ', num)
+num = 200
+print('num = ', num)
+num = 300
+print('num = ', num)
+"""
+
+"""
+num=0
+for i in range(10):
+    num += 100
+    print('ith num =',i, num)
+"""
+
+age = int(input("나이를 입력하세요:"))
+if age < 20:
+    print("청소년 할인")
+elif age >= 65:
+    print("경로 우대")
+    
+    
