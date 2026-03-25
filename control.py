ef7cb340a366467f3cefc83c901d96d81fde46d1
onestar-123 <onestar020610@naver.com>
2026-03-25 16:23:33 +0900

    Lecture

--- control.py
+++ control.py
@@ -7,17 +7,28 @@ num = 300
 print('num = ', num)
 """
 
-"""
+'''
 num=0
-for i in range(10):
+for i in range(100):
     num += 100
     print('ith num =',i, num)
-"""
+'''
 
+'''
 age = int(input("나이를 입력하세요:"))
 if age < 20:
     print("청소년 할인")
 elif age >= 65:
     print("경로 우대")
+'''
+
+'''
+hour = int(input("시간을 입력하세요:"))
+if hour < 12:
+    print("오전입니다.")
+else:
+    print("오후입니다.")
+'''
+
     
     
