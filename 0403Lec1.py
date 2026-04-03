f0a3b6e05760cee51e2e0a75713cb9f218318315
onestar-123 <onestar020610@naver.com>
2026-04-03 10:26:34 +0900

    0403Lec

---
+++ 0403Lec1.py
@@ -0,0 +1,23 @@
+i = 0
+while i < 5:
+    print('Welcome to everyone!!')
+    i+=1
+
+i = 0
+for i in range(5):
+    print('Welcome to everyone!!')
+   
+i=0    
+isum =0    
+n_list = [0, 2, 5, 10]
+for i in n_list:
+    isum += i
+print(isum)
+
+i=0
+isum = 0
+n_list = [0, 2, 5, 10]
+while i < len(n_list):
+    isum += n_list[i]
+    i+=1
+print(isum)
