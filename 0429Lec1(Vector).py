b8e0e9b62d23dfa6c3faef1a7cd7610082432b62
onestar-123 <onestar020610@naver.com>
2026-04-29 17:47:02 +0900

    0429Lec

---
+++ 0429Lec1(Vector).py
@@ -0,0 +1,32 @@
+class Vector2D:
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+        
+    def __str__(self):
+        return"({},{})".format(self.x, self.y)
+    
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y - other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return self.x*other.x+self.y*other.y
+
+    def __gt__(self,other):
+        return self.x**2 + self.y**2 > other.x**2 + other.y**2
+    
+    def __lt__(self,other):
+        return self.x**2 + self.y**2 < other.x**2 + other.y**2
+v1 = Vector2D(30, 40)
+v2 = Vector2D(10, 20)
+v3 = v1 + v2
+print('v1 + v2 =', v3)
+v4 = v1 - v2
+print('v1 - v2 =', v4)
+iprod = v1*v2
+print('v1 * v2 =', iprod)
+print(v1 > v2)
+print(v1 < v2)
